0bc9a1633fb497e31436dd20f31ede04d6ced981
DeklanMA <deklanmalik@gmail.com>
2024-03-16 13:21:51 +0700

    learning-1

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("deklan")
+	fmt.Println("deklan malik")
+	fmt.Println("deklan malik akbar")
+
+	fmt.Println(len("deklan"))
+	fmt.Println("deklan"[0]) // mengambil huruf pertama d
+	fmt.Println("deklan"[1]) // mengambil huruf kedua e
+}
---
+++ variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "deklan"
+	fmt.Println(name)
+
+	name = "deklan malik"
+	fmt.Println(name)
+
+	name = "malik akbar"
+	fmt.Println(name)
+
+	fmt.Println(len(name))
+	fmt.Println(name[0]) // mengambil huruf pertama d
+	fmt.Println(name[1]) // mengambil huruf kedua e
+}
